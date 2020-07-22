f030e6d1a22044ff85b60c0f9c686e1b0991fc0a
truongnh-91 <nguyenhuutruong.20111991@gmail.com>
2020-07-22 11:31:00 +0700

    Updates

---
+++ precache-manifest.051af24d4fc662e818c54a61d8c08626.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a66ce0090604dee0bbfd09b1938726fd",
+    "url": "/setting-color-react/index.html"
+  },
+  {
+    "revision": "09d1713e4a299bf28276",
+    "url": "/setting-color-react/static/css/main.88ebbd3d.chunk.css"
+  },
+  {
+    "revision": "01717d6ab167d860e7ac",
+    "url": "/setting-color-react/static/js/2.6f62947c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/setting-color-react/static/js/2.6f62947c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09d1713e4a299bf28276",
+    "url": "/setting-color-react/static/js/main.17387e07.chunk.js"
+  },
+  {
+    "revision": "05353a660e2be7f3dae4",
+    "url": "/setting-color-react/static/js/runtime-main.bb36809e.js"
+  }
+]);
